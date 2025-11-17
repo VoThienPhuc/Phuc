fd3b44627aa6f628cd7826dc6ec83d0eb7829fb5
VoThienPhuc <phuc.vt.67khmt@ntu.edu.vn>
2025-11-17 11:22:17 +0700

    kiem_tra_chan_le

---
+++ Kiem_tra_chan_le.py
@@ -0,0 +1,11 @@
+try:
+    so = int(input("Nhập vào một số nguyên: "))
+    if so % 2 == 0:
+        print(f"Số {so} là số chẵn")
+    else:
+        print(f"Số {so} là số lẻ")
+except ValueError:
+    print("Vui lòng nhập vào một số nguyên hợp lệ.")
+except Exception as e:
+    print(f"Đã xảy ra lỗi: {e}")
+
