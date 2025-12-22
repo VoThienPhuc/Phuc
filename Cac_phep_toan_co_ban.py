c6eae67baf66292eacdbdd9b30c6345c1fb50da6
VoThienPhuc <phuc.vt.67khmt@ntu.edu.vn>
2025-12-22 11:06:50 +0700

    Cac phep tinh co ban(sum,min,max)

---
+++ Cac_phep_toan_co_ban.py
@@ -0,0 +1,8 @@
+list = [5 , 12 , -3 , 8 , 20]
+tong = sum(list)
+print("Tổng các phần tử trong danh sách là:", tong)
+lon_nhat = max(list)
+nho_nhat = min(list)
+print(f"Lớn nhất: {lon_nhat}, Nhỏ nhất: {nho_nhat}")
+trung_binh = tong / len(list)
+print("Giá trị trung bình của các phần tử là:", trung_binh)
