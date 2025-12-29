acf3454ab8e35175c664b201b65d4d7067bc3e1c
VoThienPhuc <phuc.vt.67khmt@ntu.edu.vn>
2025-12-29 14:28:25 +0700

    tong 3 mon

---
+++ TH2912_BAI13.py
@@ -0,0 +1,14 @@
+a = float(input("Nhập vào số a: "))
+b = float(input("Nhập vào số b: "))
+c = float(input("Nhập vào số c: "))
+tong = a + b + c
+if tong >= 15 and a >= 4 and b >= 4 and c >= 4:
+    print("Đậu")
+    if a > 5 and b > 5 and c > 5:
+      print("Học đều các môn")
+    else:
+        print("Học chưa đều các môn")
+else:
+    print("thi hỏng")
+
+
